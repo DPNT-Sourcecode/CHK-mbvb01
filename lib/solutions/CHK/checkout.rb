5a3ba2fff67c2861e2168dd9385caaf48fa1d364
sbx_user1051 <sbx_user1051@169.254.237.165>
2019-04-08 14:35:04 +0000

    Mon Apr 08 14:35:04 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -13,13 +13,22 @@ end
   def checkout(skus)
     skus_array = skus.split('')
     @total = 0
+    @sku_total = {0}
     skus_array.each do |sku|
       if !@prices.has_key? sku.to_sym
         return @total = -1
       end
+      @sku_total[sku] += 1
       @total += @prices[sku.to_sym]
+    end
+    if @sku_total['A'] == 3
+      @total -= 20
+    end
+    if @sku_total['B'] == 2
+      @total -= 15
     end
     @total
   end
 
 end
+
