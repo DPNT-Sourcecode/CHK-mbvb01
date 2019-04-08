f18dfa1952b9451c364969298635017a52d4882a
sbx_user1051 <sbx_user1051@169.254.237.165>
2019-04-08 14:14:47 +0000

    Mon Apr 08 14:14:47 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -1,20 +1,24 @@
 # noinspection RubyUnusedLocalVariable
 class Checkout
 
+def initialize
   @prices = {
     A: 50,
     B: 30,
     C: 20,
     D: 15
   }
+end
   def checkout(skus)
     skus_array = skus.split('')
     @total = 0
-    skus_array.each |sku| do
+    skus_array.each do |sku|
+      p @prices[sku]
       @total += @prices[sku]
     end
     @total
   end
 
 end
+
 
