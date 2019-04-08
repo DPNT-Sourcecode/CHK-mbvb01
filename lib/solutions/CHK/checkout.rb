629e97688452e8b756c39e17b2dadcab99809923
sbx_user1051 <sbx_user1051@169.254.237.165>
2019-04-08 14:11:47 +0000

    Mon Apr 08 14:11:47 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -1,8 +1,20 @@
 # noinspection RubyUnusedLocalVariable
 class Checkout
 
+  @prices = {
+    A: 50,
+    B: 30,
+    C: 20,
+    D: 15
+  }
   def checkout(skus)
-    raise 'Not implemented'
+    skus_array = skus.split('')
+    @total = 0
+    skus_array.each |sku| do
+      @total += @prices[sku]
+    end
+    @total
   end
 
 end
+
