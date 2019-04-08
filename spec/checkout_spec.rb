72b5322d9c4f5b58385b682c8038c98afeb56e46
sbx_user1051 <sbx_user1051@169.254.237.165>
2019-04-08 14:05:46 +0000

    Mon Apr 08 14:05:46 UTC 2019

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,1 +1,12 @@
 require_relative '../lib/solutions/CHK/checkout'
+
+describe Checkout do
+
+  before :each do
+    @checkout = Checkout.new
+  end
+
+  it 'returns the value of the passed skus' do
+    expect(@checkout.checkout('AAB')).to eq (130)  
+  end
+end
